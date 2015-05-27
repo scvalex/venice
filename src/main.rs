d064609240212ce7ae1742c7f1928db3a323272d
Alexandru Scvortov <code@scvalex.net>
2015-05-27 21:59:57 +0100

    some types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Resources {
+    force: u32,
+    popularity: u32,
+    influence: u32,
+}
+
+struct Money(u32);
+
+struct Agenda {
+    id: String,
+    title: String,
+    flavour_text: String,
+    cost: Resources,
+    value: Money,
+}
+
+struct Asset {
+    id: String,
+    title: String,
+    flavour_text: String,
+    provides: Resources,
+}
+
+enum Item {
+    Agenda(Agenda),
+    Asset(Asset),
+}
+
 fn main() {
     println!("Hello, world!");
 }
