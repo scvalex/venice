e8e76b10464d291bcd4812640552ce8c1ea9c8b2
Alexandru Scvortov <code@scvalex.net>
2015-05-30 12:33:42 +0100

    command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 extern crate venice;
+extern crate docopt;
+extern crate rustc_serialize;
 
-use venice::*;
 use std::fs::File;
 
+use docopt::Docopt;
+
+use venice::*;
+
+static USAGE: &'static str = "
+Usage: venice data-pack <file>
+       venice --help
+
+Options:
+    -h, --help     Show this message.
+
+Commands:
+    data-pack   Load and print a data pack.
+
+";
+
 fn main() {
-    let dp = DataPack::load(&mut File::open("res/demo_auction.json").unwrap());
-    println!("data_pack: {}", dp);
+    let args =
+        Docopt::new(USAGE)
+        .and_then(|dopt| dopt.parse())
+        .unwrap_or_else(|e| e.exit());
+    if args.get_bool("data-pack") {
+        let dp = DataPack::load(&mut File::open(args.get_str("<file>")).unwrap());
+        println!("data_pack: {}", dp);
+    } else {
+        unreachable!();
+    }
 }
